2a75d57fc2e7cb5f4b62a47b99719b34de3ae5c4
maabaaall <miqbalrosyidin24@gmail.com>
2022-06-02 17:31:08 +0700

    first commit

---
+++ app/src/main/java/com/iqbal/utspraktik_if5_10119209_muhammadiqbalrosyidin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iqbal.utspraktik_if5_10119209_muhammadiqbalrosyidin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
